1acedf5a185ff627731afb092b790298224f0e08
paythrough <ripum.k@paythrough.in>
2024-08-27 12:11:52 +0530

    React-side-bar

---
+++ src/Pages/Service.js
@@ -0,0 +1,9 @@
+import MotionHoc from "./MotionHoc";
+
+const CalenderComponent = () => {
+  return <h1>Service</h1>;
+};
+
+const Calender = MotionHoc(CalenderComponent);
+
+export default Calender;
---
+++ src/Pages/contact.js
@@ -0,0 +1,9 @@
+import MotionHoc from "./MotionHoc";
+
+const DocumentsComponent = () => {
+  return <h1>Contact</h1>;
+};
+
+const Documents = MotionHoc(DocumentsComponent);
+
+export default Documents;
